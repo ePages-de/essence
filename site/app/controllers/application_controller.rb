e18d4c63c09083aa4885819395a4abb3bcc40bb5
Unai Abrisketa <uabrisketa@epages.com>
2024-09-26 10:50:25 +0200

    Add basic test page

--- site/app/controllers/application_controller.rb
+++ site/app/controllers/application_controller.rb
@@ -3,4 +3,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  layout 'beyond_essence/application'
 end
--- site/config/routes.rb
+++ site/config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount BeyondEssence::Engine => '/beyond_essence'
+  root to: 'home#index'
 end
