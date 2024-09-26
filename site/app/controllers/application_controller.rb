1b5e59e46ab9294ce633d5f3e5c599639bede7d5
Unai Abrisketa <uabrisketa@epages.com>
2024-09-26 14:21:21 +0200

    Update body rendering

--- site/app/controllers/application_controller.rb
+++ site/app/controllers/application_controller.rb
@@ -3,6 +3,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  layout 'beyond_essence/application'
 end
