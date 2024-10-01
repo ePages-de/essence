7231042ef872264cf1e07b00552a12dabc1a0f6b
Unai Abrisketa <uabrisketa@epages.com>
2024-10-01 14:29:27 +0200

    Add scroll-shadow-element

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin '@floating-ui/dom', to: '@floating-ui--dom.js' # @1.6.11
 pin '@floating-ui/core', to: '@floating-ui--core.js' # @1.6.8
 pin '@floating-ui/utils', to: '@floating-ui--utils.js' # @0.2.8
 pin '@floating-ui/utils/dom', to: '@floating-ui--utils--dom.js' # @0.2.8
+pin 'scroll-shadow-element' # @2.0.5
