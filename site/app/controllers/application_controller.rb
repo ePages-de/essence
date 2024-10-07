2eb2e905aa5ff99a839d66c48f7cb31ffb7e90c8
Unai Abrisketa <uabrisketa@epages.com>
2024-10-07 15:10:51 +0200

    Save

--- site/app/controllers/application_controller.rb
+++ site/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
