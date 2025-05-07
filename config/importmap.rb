4a9ad7a0b3b29b6c73b7331d70e5c07e62cab168
Unai Abrisketa <uabrisketa@epages.com>
2025-05-07 13:01:06 +0200

    Fix importmap by specifying the correct pin for essence/application

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-pin 'application', to: 'essence/application.js', preload: true
+pin 'essence/application', to: 'essence/application.js', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
