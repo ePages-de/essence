15a5975f247a17fe406cd81800d98864944479b9
Unai Abrisketa <uabrisketa@epages.com>
2025-05-11 19:19:33 +0200

    Refactor ButtonGroupComponent to support multiple element types and update rendering logic

--- app/components/essence/button_group_component.rb
+++ app/components/essence/button_group_component.rb
@@ -4,11 +4,20 @@ module Essence
   class ButtonGroupComponent < ApplicationComponent
     attr_reader :html_options
 
-    renders_many :buttons, ButtonComponent
+    renders_many :group, types: {
+      button: lambda { |**args|
+        ButtonComponent.new(size: 16, **args.merge(display_as: :button))
+      },
+      link: lambda { |**args|
+        LinkComponent.new(size: 16, **args.merge(display_as: :button))
+      }
+    }
 
     def initialize(**html_options)
       @html_options = html_options
     end
+
+    alias button_group_elements group
 
     private
 
