f263ead5adf3e94e56c80a8a79e6f4b568783889
Unai Abrisketa <uabrisketa@epages.com>
2025-06-27 13:14:24 +0200

    Add ribbon to cards

--- app/components/essence/card_component.rb
+++ app/components/essence/card_component.rb
@@ -4,12 +4,44 @@ module Essence
   class CardComponent < ApplicationComponent
     attr_reader :headline, :sub_headline, :html_options
 
+    renders_one :ribbon, 'RibbonComponent'
+
     def initialize(headline: nil,
                    sub_headline: nil,
                    **html_options)
-      @headline     = headline
-      @sub_headline = sub_headline
-      @html_options = html_options
+      @headline       = headline
+      @sub_headline   = sub_headline
+      @html_options   = html_options
+    end
+
+    class RibbonComponent < ApplicationComponent
+      attr_reader :text, :html_options
+
+      DEFAULT_VARIANT = :light
+
+      VARIANT_MAPPINGS = {
+        DEFAULT_VARIANT => 'card-ribbon-light',
+        :dark => 'card-ribbon-dark'
+      }.freeze
+
+      VARIANT_OPTIONS = VARIANT_MAPPINGS.keys.freeze
+
+      def initialize(text:,
+                     variant: DEFAULT_VARIANT,
+                     **html_options)
+        @text         = text
+        @variant      = fetch_or_fallback(VARIANT_OPTIONS, variant.to_sym, DEFAULT_VARIANT)
+        @html_options = html_options
+      end
+
+      private
+
+      def before_render
+        set_base_html_options(
+          'card-ribbon',
+          VARIANT_MAPPINGS[@variant]
+        )
+      end
     end
 
     private
