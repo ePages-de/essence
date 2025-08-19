4bc3ecb37e6f54593f685787d56a32aed7f4f594
Unai Abrisketa <uabrisketa@epages.com>
2025-08-19 13:52:06 +0200

    Ad busy state to buttons

--- app/javascript/essence/controllers/index.js
+++ app/javascript/essence/controllers/index.js
@@ -5,14 +5,17 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import ParagraphComponentController from 'components/essence/paragraph_component/paragraph_component_controller'
-application.register('paragraph', ParagraphComponentController)
+import ButtonComponentController from 'components/essence/button_component/button_component_controller'
+application.register('button', ButtonComponentController)
 
 import ClipboardCopyComponentController from 'components/essence/clipboard_copy_component/clipboard_copy_component_controller'
 application.register('clipboard-copy', ClipboardCopyComponentController)
 
 import FlashComponentController from 'components/essence/flash_component/flash_component_controller'
 application.register('flash', FlashComponentController)
+
+import ParagraphComponentController from 'components/essence/paragraph_component/paragraph_component_controller'
+application.register('paragraph', ParagraphComponentController)
 
 import TooltipComponentController from 'components/essence/tooltip_component/tooltip_component_controller'
 application.register('tooltip', TooltipComponentController)
