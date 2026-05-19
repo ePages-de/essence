d9d87a6e4220b6fadc1ab92bf15345b4d6eefa09
kengallego <kgallego@epages.com>
2026-05-19 12:29:46 +0200

    fix: pass merged_input_options to check_box in ToggleInput
    The check_box was called with empty options, losing the form attribute
    needed when the input is defined outside the form tag via HTML5 form
    association. This worked in ViewComponent v3 due to the ActionView::Base
    capture compatibility patch, which was removed in v4.

--- app/inputs/essence/toggle_input.rb
+++ app/inputs/essence/toggle_input.rb
@@ -6,7 +6,7 @@ module Essence
       merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
 
       @builder.label(attribute_name, merged_input_options) { '' } +
-        build_check_box(unchecked_value, {})
+        build_check_box(unchecked_value, merged_input_options)
     end
   end
 end
